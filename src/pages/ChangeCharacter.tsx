fa35b0b9d880bad1777db418a83e42ccd25c8c56
제일 <joimh7@gmail.com>
2024-08-02 19:27:46 +0900

    아무것도 없을 때는 불릿도 제거

--- src/pages/ChangeCharacter.tsx
+++ src/pages/ChangeCharacter.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
+import ReactDOMServer from 'react-dom/server';
 import "./style/ChangeCharacter.css";
 import styled, { StyledComponent } from "@emotion/styled";
-import ReactDOMServer from 'react-dom/server';
 
 const 테스트스타일컴포넌트 = styled.span`
     color: orange;
@@ -19,48 +19,58 @@ const ChangeCharacter = () => {
 
     interface ParseTextProps {
         text: string;
-        FontColorWrapper: StyledComponent<"span", any>;
-        BoldAndColoredWrapper: StyledComponent<"span", any>;
+        bulletOption: boolean;
+        FontColorWrapper: React.ComponentType<any>;
+        BoldAndColoredWrapper: React.ComponentType<any>;
     }
 
-    const parseText = ({ text, FontColorWrapper, BoldAndColoredWrapper }: ParseTextProps) => {
+    const parseText = ({ text, bulletOption = true, FontColorWrapper, BoldAndColoredWrapper }: ParseTextProps) => {
         const parts = text.split(/\n/g); // Split by newline character
 
         return parts.map((part, index) => {
+            // 문자열 내에서 ^로 감싸진 부분과 %로 감싸진 부분을 분리
             const innerParts = part.split(/(\^[^^]+\^|%[^%]+%|\^[^^]+%[^%]+\^|\^[^^]+%[^%]+%)/g);
+            // 빈 문자열을 제거
+            const filteredInnerParts = innerParts.filter(part => part.trim() !== '');
+
+            console.log(filteredInnerParts);
 
             return (
                 <React.Fragment key={index}>
-                    <span>&bull; </span> {/* Bullet point */}
-                    {innerParts.map((innerPart, innerIndex) => {
-                        if (innerPart.startsWith("^") && innerPart.endsWith("^")) {
-                            if (innerPart.includes("%")) {
-                                const content = innerPart.replace(/\^|%/g, '');
-                                return (
-                                    <BoldAndColoredWrapper key={innerIndex}>
-                                        {content}
-                                    </BoldAndColoredWrapper>
-                                );
-                            } else {
-                                const content = innerPart.slice(1, -1);
-                                return (
-                                    <FontColorWrapper key={innerIndex}>
-                                        {content}
-                                    </FontColorWrapper>
-                                );
-                            }
-                        } else if (innerPart.startsWith("%") && innerPart.endsWith("%")) {
-                            const content = innerPart.slice(1, -1);
-                            return (
-                                <span key={innerIndex} className="bold">
-                                    {content}
-                                </span>
-                            );
-                        } else {
-                            return <span key={innerIndex}>{innerPart}</span>;
-                        }
-                    })}
-                    <br /> {/* Line break for each part */}
+                    {filteredInnerParts.length > 0 ? (
+                        <>
+                            {bulletOption && <span>&bull; </span>}
+                            {filteredInnerParts.map((innerPart, innerIndex) => {
+                                if (innerPart.startsWith("^") && innerPart.endsWith("^")) {
+                                    if (innerPart.includes("%")) {
+                                        const content = innerPart.replace(/\^|%/g, '');
+                                        return (
+                                            <BoldAndColoredWrapper key={innerIndex}>
+                                                {content}
+                                            </BoldAndColoredWrapper>
+                                        );
+                                    } else {
+                                        const content = innerPart.slice(1, -1);
+                                        return (
+                                            <FontColorWrapper key={innerIndex}>
+                                                {content}
+                                            </FontColorWrapper>
+                                        );
+                                    }
+                                } else if (innerPart.startsWith("%") && innerPart.endsWith("%")) {
+                                    const content = innerPart.slice(1, -1);
+                                    return (
+                                        <span key={innerIndex} className="bold">
+                                            {content}
+                                        </span>
+                                    );
+                                } else {
+                                    return <span key={innerIndex}>{innerPart}</span>;
+                                }
+                            })}
+                        </>
+                    ) : null}
+                    <br /> {/* 항상 개행 유지 */}
                 </React.Fragment>
             );
         });
@@ -87,13 +97,12 @@ const ChangeCharacter = () => {
             </p>
             <hr className="my-5" />
             <div className="mt-5">
-                {parseText({ text: 특수문자들어간문자열, FontColorWrapper: 테스트스타일컴포넌트, BoldAndColoredWrapper: BoldAndColoredComponent })}
+                {parseText({ text: 특수문자들어간문자열, bulletOption: true, FontColorWrapper: 테스트스타일컴포넌트, BoldAndColoredWrapper: BoldAndColoredComponent })}
             </div>
             <br />
             <br />
-            <br />
             <div>
-                {parseText({ text: 특수문자들어간문자열, FontColorWrapper: 테스트스타일컴포넌트, BoldAndColoredWrapper: BoldAndColoredComponent }).map((element, index) => (
+                {parseText({ text: 특수문자들어간문자열, bulletOption: true, FontColorWrapper: 테스트스타일컴포넌트, BoldAndColoredWrapper: BoldAndColoredComponent }).map((element, index) => (
                     <pre key={index}>
                         {React.isValidElement(element)
                             ? ReactDOMServer.renderToStaticMarkup(element)
